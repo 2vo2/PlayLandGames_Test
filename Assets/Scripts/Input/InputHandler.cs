455e5a32d9b55d35ac3634138d4d2383a5cbf1f3
2vo2 <91755408+2vo2@users.noreply.github.com>
2026-06-04 09:12:14 +0300

    feat: create player mover

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -1,15 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
     private InputSystem_Actions _inputActions;
 
-    private event Action OnJumpButtonPressed;
+    public event Action OnJumpButtonPressed;
 
-    private Vector2 _inputVector;
+    private Vector2 _moveInputVector;
 
-    public Vector2 InputVector => _inputVector;
+    public Vector2 MoveInputVector => _moveInputVector;
 
     private void Awake()
     {
@@ -19,14 +20,31 @@ public class InputHandler : MonoBehaviour
     private void OnEnable()
     {
         _inputActions.Enable();
-        _inputActions.Player.Move.performed += context => {_inputVector = context.ReadValue<Vector2>(); print(_inputVector);};
-        _inputActions.Player.Jump.performed += context => OnJumpButtonPressed?.Invoke();
+        _inputActions.Player.Move.started += MoveInputStarted;
+        _inputActions.Player.Move.canceled += MoveInputCanceled;
+        _inputActions.Player.Jump.performed += JumpButtonPressed;
     }
 
     private void OnDisable()
     {
-        _inputActions.Player.Move.performed -= context => print(context.ReadValue<Vector2>());
-        _inputActions.Player.Jump.performed -= context => OnJumpButtonPressed?.Invoke(); 
+        _inputActions.Player.Move.started -= MoveInputStarted;
+        _inputActions.Player.Move.canceled -= MoveInputCanceled;
+        _inputActions.Player.Jump.performed -= JumpButtonPressed; 
         _inputActions.Disable();
+    }
+
+    private void MoveInputStarted(InputAction.CallbackContext context)
+    {
+        _moveInputVector = context.ReadValue<Vector2>();
+    }
+
+    private void MoveInputCanceled(InputAction.CallbackContext context)
+    {
+        _moveInputVector = Vector2.zero;
+    }
+
+    private void JumpButtonPressed(InputAction.CallbackContext context)
+    {
+        OnJumpButtonPressed?.Invoke();
     }
 }
